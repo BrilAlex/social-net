67ace89078da52bf41bee9c132940a05c89d70a4
BrilAlex <brilalex.it@yandex.ru>
2022-02-04 19:31:11 +0300

    Lesson 22 Route exact done

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,17 +1,42 @@
 import React from "react";
 import styles from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 type DialogsPropsType = {};
 
 export const Dialogs = (props: DialogsPropsType) => {
+  const getNavLinkClassName = (navData: {isActive: boolean}) => {
+    return navData.isActive ? styles.active : "";
+  };
+
   return (
     <div className={styles.dialogsPage}>
       <div className={styles.dialogsContainer}>
-        <div className={styles.dialog + " " + styles.active}>Karina</div>
-        <div className={styles.dialog}>Dimych</div>
-        <div className={styles.dialog}>Sergei</div>
-        <div className={styles.dialog}>Alexander</div>
-        <div className={styles.dialog}>Svetlana</div>
+        <div className={styles.dialog + " " + styles.active}>
+          <NavLink to={"/dialogs/1"} className={(navData) => getNavLinkClassName(navData)}>
+            Karina
+          </NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to={"/dialogs/2"} className={(navData) => getNavLinkClassName(navData)}>
+            Dimych
+          </NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to={"/dialogs/3"} className={(navData) => getNavLinkClassName(navData)}>
+            Sergei
+          </NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to={"/dialogs/4"} className={(navData) => getNavLinkClassName(navData)}>
+            Alexander
+          </NavLink>
+        </div>
+        <div className={styles.dialog}>
+          <NavLink to={"/dialogs/5"} className={(navData) => getNavLinkClassName(navData)}>
+            Svetlana
+          </NavLink>
+        </div>
       </div>
       <div className={styles.messagesContainer}>
         <div className={styles.message}>
